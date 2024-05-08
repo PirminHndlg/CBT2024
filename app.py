1d7282d242a4158fab4d867484b1f786d0f354da
Pirmin <pirmin@absinth.net>
2024-05-08 23:55:59 +0200

    added lang

--- app.py
+++ app.py
@@ -16,31 +16,40 @@ def get_element(nr):
     return return_data
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<lang>')
+def index(lang=None):
+    if not lang:
+        device_lang = request.accept_languages.best_match(['de', 'hu', 'pl', 'cz', 'sk'])
+        if device_lang:
+            return redirect(url_for('index', lang=device_lang))
+    return render_template('index.html', lang=lang)
 
+@app.route('/<lang>/programm/<int:point>')
 @app.route('/programm/<int:point>')
-def programm_point(point):
+def programm_point(lang='de', point=None):
 
     if point:
-        return render_template(f'programm_point.html', section=get_element(point))
+        return render_template(f'programm_point.html', lang=lang, section=get_element(point))
     return redirect(url_for('programm'))
 
+@app.route('/<lang>/section/<section>')
 @app.route('/section/<section>')
-def programm_section(section):
+def programm_section(lang='de', section=None):
     if section:
-        return render_template(f'programm_section.html', section=section)
+        return render_template(f'programm_section.html', lang=lang, section=section)
     return redirect(url_for('programm'))
 
 
+@app.route('/<lang>/programm')
 @app.route('/programm')
+@app.route('/<lang>/section')
 @app.route('/section')
-def programm():
-    return render_template('programm.html')
+def programm(lang='de'):
+    return render_template('programm.html', lang=lang)
 
-@app.route('/my')
-def my_programm():
-    return render_template('my_programm.html')
+@app.route('/<lang>/my-program')
+def my_programm(lang='de'):
+    return render_template('my_programm.html', lang=lang)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
